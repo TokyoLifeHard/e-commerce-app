c2dbce1e31a282e79098d0f1519e47dc88b68285
Kawaiiskii <antonzhadnov@gmail.com>
2025-01-19 22:10:16 +0300

    init category entity

--- services/product-service/src/main/java/com/ecommpet/product/entity/Product.java
+++ services/product-service/src/main/java/com/ecommpet/product/entity/Product.java
@@ -1,4 +1,25 @@
 package com.ecommpet.product.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    private Category category;
 }
