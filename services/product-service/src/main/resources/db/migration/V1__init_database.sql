7c6d79be5422955ebb335663a059f79113d553b3
Kawaiiskii <antonzhadnov@gmail.com>
2025-01-22 01:27:53 +0300

    fix mistakes

--- services/product-service/src/main/resources/db/migration/V1__init_database.sql
+++ services/product-service/src/main/resources/db/migration/V1__init_database.sql
@@ -14,7 +14,7 @@ create table if not exists product
     price numeric(38, 2),
     category_id integer
                 constraint fk_category references category
-)
+);
 
 create sequence if not exists category_seq increment by 50;
 create sequence if not exists product_seq increment by 50;
